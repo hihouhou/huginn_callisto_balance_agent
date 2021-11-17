b032b8c3b7929b990c8797443e057987801baf22
Nicolas Germain <ngermain@hihouhou.com>
2021-11-17 15:20:50 +0100

    query updated and real value with decimal added

--- lib/huginn_callisto_balance_agent/callisto_balance_agent.rb
+++ lib/huginn_callisto_balance_agent/callisto_balance_agent.rb
@@ -10,6 +10,10 @@ module Agents
       <<-MD
       The Callisto balance agent fetches callisto's balance from callisto explorer
 
+      `debug` is used to verbose mode.
+
+      `decimal` for token decimal.
+
       `expected_receive_period_in_days` is used to determine if the Agent is working. Set it to the maximum number of days
       that you anticipate passing without this Agent receiving an incoming Event.
       MD
@@ -17,29 +21,44 @@ module Agents
 
     event_description <<-MD
       Events look like this:
-        {
-          "balance": xxxx,
-          "balanceUSD": xxxx,
-          "address": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-          "crypto": "CLO"
-        }
+
+          {
+            "message": "OK",
+            "result": "8000000203531297540461059",
+            "status": "1",
+            "address": "0xXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+            "crypto": "CLO",
+            "value": 8000000.2035312975404
+          }
     MD
 
     def default_options
       {
+        'debug' => 'false',
         'wallet_address' => '',
+        'decimal' => '18',
         'expected_receive_period_in_days' => '2',
         'changes_only' => 'true'
       }
     end
 
+    form_configurable :debug, type: :boolean
     form_configurable :expected_receive_period_in_days, type: :string
     form_configurable :wallet_address, type: :string
+    form_configurable :decimal, type: :string
     form_configurable :changes_only, type: :boolean
 
     def validate_options
       unless options['wallet_address'].present?
         errors.add(:base, "wallet_address is a required field")
+      end
+
+      unless options['decimal'].present?
+        errors.add(:base, "decimal is a required field")
+      end
+
+      if options.has_key?('debug') && boolify(options['debug']).nil?
+        errors.add(:base, "if provided, debug must be true or false")
       end
 
       if options.has_key?('changes_only') && boolify(options['changes_only']).nil?
@@ -63,42 +82,38 @@ module Agents
 
     def handle(wallet)
 
-        uri = URI.parse("https://explorer.callisto.network/web3relay")
-        request = Net::HTTP::Post.new(uri)
-        request.content_type = "application/json;charset=UTF-8"
-        request["Authority"] = "explorer.callisto.network"
-        request["Accept"] = "application/json, text/plain, */*"
-        request["Origin"] = "https://explorer.callisto.network"
-        request["Sec-Fetch-Site"] = "same-origin"
-        request["Sec-Fetch-Mode"] = "cors"
-        request["Sec-Fetch-Dest"] = "empty"
-        request["Referer"] = "https://explorer.callisto.network/addr/#{wallet}"
-        request["Accept-Language"] = "fr,en-US;q=0.9,en;q=0.8"
-        request.body = '{"addr":"' + wallet + '","options":["balance"]}'
+      uri = URI.parse("https://explorer.callisto.network/api?module=account&action=balance&address=#{wallet}")
+      request = Net::HTTP::Get.new(uri)
+      request["Accept"] = "application/json"
 
-        req_options = {
-          use_ssl: uri.scheme == "https",
-        }
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
 
-        response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-          http.request(request)
-        end
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
 
-        payload = response.body
-        payload = JSON.parse(payload)
-        payload.merge!({ :address => wallet, :crypto => "CLO" })
+      payload = response.body
+      if interpolated['debug'] == 'true'
+        log payload
+      end
+      payload = JSON.parse(payload)
+      power = 10 ** interpolated['decimal'].to_i
+      value = payload['result'].to_f / power.to_i
+      payload.merge!({ :address => wallet, :crypto => "CLO", :value => value })
 
-        if interpolated['changes_only'] == 'true'
-          if payload.to_s != memory['last_status']
-            memory['last_status'] = payload.to_s
-            create_event payload: payload
-          end
-        else
+      if interpolated['changes_only'] == 'true'
+        if payload.to_s != memory['last_status']
+          memory['last_status'] = payload.to_s
           create_event payload: payload
-          if payload.to_s != memory['last_status']
-            memory['last_status'] = payload.to_s
-          end
         end
+      else
+        create_event payload: payload
+        if payload.to_s != memory['last_status']
+          memory['last_status'] = payload.to_s
+        end
+      end
     end
   end
 end
